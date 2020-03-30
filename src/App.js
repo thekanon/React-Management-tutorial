b1f59d24267bb81a68cdb6333aef9c40e73de199
Doo Lee <dlengjs12@naver.com>
2020-03-31 01:02:16 +0900

    Create Customer.js & Class test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+const customers = [
+  {
+    'id': 1,
+    'image':'https://placeimg.com/64/64/1',
+    'name':'홍1길동',
+    'birthday':'99q9',
+    'gender':'woman',
+    'etc':"딸기"
+  },
+  {
+    'id': 2,
+    'image':'https://placeimg.com/64/64/2',
+    'name':'홍1길2동',
+    'birthday':'9a99',
+    'gender':'man',
+    'etc':"딸기"
+  },
+  {
+    'id': 3,
+    'image':'https://placeimg.com/64/64/3',
+    'name':'홍2길동3',
+    'birthday':'912399',
+    'gender':'ma1n',
+    'etc':"딸기"
+  }
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system</h2>
+    <div>
+      {
+        customers.map(c=> {
+          return(
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              etc={c.etc}
+            />
+          )
+        })
+      }
     </div>
   );
 }
