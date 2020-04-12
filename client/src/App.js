72e725314f369d6b34806ef358d1955b33b0dbee
Doo Lee <dlengjs12@naver.com>
2020-04-12 20:07:00 +0900

    로딩 구현

--- client/src/App.js
+++ client/src/App.js
@@ -8,45 +8,62 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import Table from '@material-ui/core/Table';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
-  root:{
-    width: '100%',
-    marginTop:theme.spacing.unit * 3,
-    overflowX: "auto"
-  },
-  table:{
-    minWidth: 1080
-  }
+    root:{
+        width: '100%',
+        marginTop:theme.spacing(3),
+        overflowX: "auto"
+    },
+        table:{
+        minWidth: 1080
+    }
+    ,progress:{
+        margin:theme.spacing(2)
+    }
 })
 
 class App extends React.Component {
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   }
   componentDidMount(){
+    this.timer = setInterval(this.progress, 20);
     this.callAPI()
       .then(res => this.setState({customers: res}))
       .catch(err => console.log(err) );
   }
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }  
   callAPI = async () => {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
   }
+
+  progress = () => {
+      const { completed } = this.state;
+      this.setState({ completed: completed >= 100 ? 0 : completed +1});
+  }
+
   render() {
     const { classes } = this.props; 
     return ( 
       <Paper className={classes.root}> 
         <Table className={classes.table}>
           <TableHead>
-            <TableCell>번호</TableCell>
-            <TableCell>그림</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>사번</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>과일</TableCell>
+            <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>그림</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>사번</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>과일</TableCell>
+            </TableRow>
           </TableHead>
           <TableBody>
           {
@@ -62,9 +79,13 @@ class App extends React.Component {
                   etc={c.etc}
                 />
               )
-            }) : ""
+            }) : 
+            <TableRow>
+                <TableCell colSpan="6" align="center">
+                    <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+                </TableCell>
+            </TableRow>
           }
-
           </TableBody>
         </Table>
       </Paper>  
--- server.js
+++ server.js
@@ -7,34 +7,36 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true}));
 
 app.get('/api/customers',(req,res)=> {
-    res.send(
-        [
-            {
-              'id': 1,
-              'image':'https://placeimg.com/64/64/1',
-              'name':'홍1길동',
-              'birthday':'99q9',
-              'gender':'woman',
-              'etc':"딸기"
-            },
-            {
-              'id': 2,
-              'image':'https://placeimg.com/64/64/2',
-              'name':'홍1길2동',
-              'birthday':'9a99',
-              'gender':'man',
-              'etc':"딸기"
-            },
-            {
-              'id': 3,
-              'image':'https://placeimg.com/64/64/3',
-              'name':'홍2길동3',
-              'birthday':'912399',
-              'gender':'ma1n',
-              'etc':"딸기"
-            }
-          ]        
-    )
+    setTimeout(function() {
+        res.send(
+                [
+                    {
+                    'id': 1,
+                    'image':'https://placeimg.com/64/64/1',
+                    'name':'최대공약수와 최소공배수',
+                    'birthday':'99q9',
+                    'gender':'woman',
+                    'etc':"딸기"
+                    },
+                    {
+                    'id': 2,
+                    'image':'https://placeimg.com/64/64/2',
+                    'name':'홍1길2동',
+                    'birthday':'9a99',
+                    'gender':'man',
+                    'etc':"딸기"
+                    },
+                    {
+                    'id': 3,
+                    'image':'https://placeimg.com/64/64/3',
+                    'name':'홍2길동3',
+                    'birthday':'912399',
+                    'gender':'ma1n',
+                    'etc':"딸기"
+                    }
+            ]        
+        )
+    },2000);
 })
 
 app.listen(port, () => console.log(`listen on port ${port}`));
