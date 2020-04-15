4b59b75977642fae4dd8ab9ee3aede884f1bca09
Doo Lee <dlengjs12@naver.com>
2020-04-16 00:21:56 +0900

    간단한 폼 만들기

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import CustomerAdd from './components/CustomerAdd';
 import Paper from '@material-ui/core/Paper';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
@@ -52,42 +53,45 @@ class App extends React.Component {
   render() {
     const { classes } = this.props; 
     return ( 
-      <Paper className={classes.root}> 
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-                <TableCell>번호</TableCell>
-                <TableCell>그림</TableCell>
-                <TableCell>이름</TableCell>
-                <TableCell>사번</TableCell>
-                <TableCell>성별</TableCell>
-                <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-          {
-            this.state.customers ? this.state.customers.map(c=> {
-              return(
-                <Customer
-                  key={c.id}
-                  id={c.id}
-                  image={c.image}
-                  name={c.name}
-                  birthday={c.birthday}
-                  gender={c.gender}
-                  etc={c.etc}
-                />
-              )
-            }) : 
-            <TableRow>
-                <TableCell colSpan="6" align="center">
-                    <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
-                </TableCell>
-            </TableRow>
-          }
-          </TableBody>
-        </Table>
-      </Paper>  
+    <div>
+        <Paper className={classes.root}> 
+            <Table className={classes.table}>
+            <TableHead>
+                <TableRow>
+                    <TableCell>번호</TableCell>
+                    <TableCell>그림</TableCell>
+                    <TableCell>이름</TableCell>
+                    <TableCell>사번</TableCell>
+                    <TableCell>성별</TableCell>
+                    <TableCell>직업</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+            {
+                this.state.customers ? this.state.customers.map(c=> {
+                return(
+                    <Customer
+                    key={c.id}
+                    id={c.id}
+                    image={c.image}
+                    name={c.name}
+                    birthday={c.birthday}
+                    gender={c.gender}
+                    etc={c.etc}
+                    />
+                )
+                }) : 
+                <TableRow>
+                    <TableCell colSpan="6" align="center">
+                        <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+                    </TableCell>
+                </TableRow>
+            }
+            </TableBody>
+            </Table>
+        </Paper>  
+        <CustomerAdd />
+      </div>
     );
   }
 }
